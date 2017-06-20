6f4e9251a49b0819abc45471e624d44430f31cf0
ryuxik <smunoz@mit.edu>
2017-06-20 13:30:39 -0400

    changed the test and added a new file to contain mem map testing

--- UROP_NODE/RPI_a_test.py
+++ UROP_NODE/RPI_a_test.py
@@ -6,17 +6,18 @@
 """
 import test.busComm as comm
 import test.fpga_link_test as flink
+import test.mmap_test.py as mmap
+from restruct.node import NodeFPGA 
 
 if __name__ == "__main__":
 	testsPassed = False
-	vid_pid = 101 # replace with proper vendor and product id of FPGA board
+	vid_pid_did = '1:0:1' # replace with proper vendor and product id and device id of FPGA board
 	debug_level = 0 #replace with desired debug level for fl API
-	busONE_directory = "b1" #replace with proper dir
-	busTWO_directory = 'b2' #replace with proper dir
-	camera_directory = "c" #replace with proper dir
-	modulator_directory = "m" #replace with proper dir
-	slave_switch_directory = 'ss' #replace with proper dir
-	master_switch_directory = 'ms' #replace with proper dir
+	busONE_directory = "b1" #replace with proper dir, represents intial directory of bus to master usb port
+	busTWO_directory = 'b2' #replace with proper dir, represents directory of bus to slave usb hub
+	camera_directory = "c" #replace with proper dir, repesents directory of camera to slave usb hub
+	usb1_switch_directory = 'ss' #replace with proper dir, represents directory of switch for usb1.1
+	usb2_switch_directory = 'ms' #replace with proper dir, represents directory of switch for usb2.0
 	results = [] #array containing test results
 
 	#Making instances of necessary connections for test
@@ -24,50 +25,49 @@ if __name__ == "__main__":
 	busONE = comm.serialW(busONE_directory,True)
 	busTWO = comm.serialW(busONE_directory)
 	cam = comm.serialW(camera_directory)
-	mod = comm.serialW(modulator_directory)
-	ss = comm.serialW(slave_switch_directory)
-	ms = comm.serialW(master_switch_directory)
+	uONE = comm.serialW(usb1_switch_directory)
+	uTWO = comm.serialW(usb2_switch_directory)
+	mem = mmap.tester(args)
 
-	fpga = flink.board(vid_pid, debug_level, None)
+	init_fpga_board_tester = flink.board(vid_pid_did, debug_level, None)
 
 	#Testing intial connection to bus
 	try:
-		#busONE.testConnection()
-		r = fpga.init_com_test()
-		results.extend(r)
+		busONE.testConnection()
 	except:
 		results.append('F','Initial connection to bus failed')
+
+	#Testing connection to camera
+	try:
+		cam.testConnection()
+	except:
+		results.append('F','Connection to camera failed')
+
+	#Testing connection to FPGA board
+	try:
+		results.extend(init_fpga_board_tester.init_com_test()) #run tests and return list of to extend results report
+	except:
+		results.append('F','FPGA inital connection test failed to run.')
+	
+	#Make tests using memmory map here!!
+	#or implement those tests as part of the connection tests for fpga
+	try:
+		results.extend(mem.test())
+	except:
+		results.append('F','Memmory map command testing to FPGA test failed to run.')
 
 	#Testing switch of bus to slave
 	try:
 		#implement a slave test here!!
-		results.append('P','Switch of bus to slave successfull')
+		pass
 	except:
 		results.append('F','Switch of bus to slave failed')
 		
 	#Testing connection to bus as slave
 	try:
 		busTWO.testConnection()
-		results.append('P','Connection to bus as slave successfull')
 	except:
 		results.append('F','Connection to bus as slave failed')
-
-	#Testing connection to camera
-	try:
-		cam.testConnection()
-		results.append('P','Connection to camera successfull')
-	except:
-		results.append('F','Connection to camera failed')
-
-	#Testing connection to modulator
-	try:
-		mod.testConnection()
-		results.append('P','Connection to modulator successfull')
-	except:
-		results.append('F','Connection to modulator failed')
-
-	#Make tests using memmory map here!!
-	#or implement those tests as part of the connection tests
 
 	#print(results) #see results of tests
 
---
+++ UROP_NODE/test/mmap_test.py
@@ -0,0 +1,14 @@
+"""
+@Author: Santiago Munoz
+@Date: 6/20/2017
+
+	This is to test the comm to the FPGA using the memmory map.
+"""
+
+class tester(object):
+	"""docstring for tester"""
+	def __init__(self, arg):
+		self.arg = arg
+
+	def test():
+		pass
