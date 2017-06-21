66b92cfea46d947f54f4e21e3b6238cf42c3d98e
ryuxik <smunoz@mit.edu>
2017-06-21 16:04:01 -0400

    added some comments

--- UROP_NODE/restruct/control.py
+++ UROP_NODE/restruct/control.py
@@ -1,6 +1,27 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2009-2014 Chris McClelland
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 """
 This is the consolidated control file, includes funcitons from 
 nodectrl, nodectrl_oven_test, and SPI_test
+
+NODE FPGA Commander Copyright (C) 2015 Ryan Kingsbury
+-Updated and restructured by Santiago Munoz, and Ethan Munden 2017
 """
 import fl
 import argparse
