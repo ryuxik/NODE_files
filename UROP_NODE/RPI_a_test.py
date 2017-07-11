9f846c4563d17efd4bc08f26ded166bf755e126b
ryuxik <smunoz@mit.edu>
2017-07-11 12:37:14 -0400

    added a file to update constants separately

--- UROP_NODE/RPI_a_test.py
+++ UROP_NODE/RPI_a_test.py
@@ -9,49 +9,37 @@ import test.busComm as comm
 import test.fpga_link_test as flink
 import test.mmap_test.py as mmap
 from restruct.node import NodeFPGA
-import rpiControls.pinController as pinC 
+import rpiControls.pinController as pinC
+from test.constants import ALL_C
 
 if __name__ == "__main__":
 	testsPassed = False
-	progConfig = '' #replace with FPGA config file
-	fpga_old_vid_pid = '1:0:1' # replace with proper vendor and product id and device id of FPGA board
-	new_vid_pid = ''
-	debug_level = 0 #replace with desired debug level for fl API
 	
 	## find out what type of transfer each connection is (control, isochronous, interrupt, or bulk)
 	#pl Bus info required for usb connection
-	plBus_vid = ''
-	plBus_pid = ''
-	plBus_packet_size = ''
-	plBus_timeout = ''
+	
 	plBus_master_sendTest = ''
 	plBus_master_expectTest = ''
 	plBus_slave_sendTest = ''
 	plBus_slave_expectTest = ''
-	plBus_wendpoint = ''
-	plBus_rendpoint = ''
+	
 	
 	#camera info required for usb connection
-	camera_vid = ''
-	camera_pid = ''
-	camera_packet_size = ''
-	camera_timeout = ''
+	
 	camera_sendTest = ''
 	camera_expectTest = ''
-	camera_wendpoint = ''
-	camera_rendpoint = ''
-
+	
 	results = [] #array containing test results
 
 	#Making instances of necessary connections for test
 	
 	##need to find out what type of usb transfer to use for each of these
-	plBusMaster = comm.connection(plBus_vid,plBus_pid,plBus_packet_size,plBus_timeout, plBus_wendpoint, plBus_rendpoint True)
-	camera = comm.connection(camera_vid,camera_pid,camera_packet_size,camera_timeout, camera_wendpoint, camera_rendpoint)
+	plBusMaster = comm.connection(ALL_C['plBus_vid'],ALL_C['plBus_pid'],ALL_C['plBus_packet_size'],ALL_C['plBus_timeout'], ALL_C['plBus_wendpoint'], ALL_C['plBus_rendpoint'], True)
+	camera = comm.connection(ALL_C['camera_vid'],ALL_C['camera_pid'],ALL_C['camera_packet_size'],ALL_C['camera_timeout'], ALL_C['camera_wendpoint'], ALL_C['camera_rendpoint'])
 	
-	mem = mmap.tester(fpga_old_vid_pid, fpga_new_vid_pid)
+	mem = mmap.tester(ALL_C['fpga_old_vid_pid'], ALL_C['fpga_new_vid_pid'])
 
-	init_fpga_board_tester = flink.board(fpga_old_vid_pid_did, debug_level, None)
+	init_fpga_board_tester = flink.board(ALL_C['fpga_old_vid_pid'], ALL_C['debug_level'], None)
 
 	#Testing intial connection to bus
 	try:
@@ -73,7 +61,7 @@ if __name__ == "__main__":
 	
 	#Make tests using memmory map here!!
 	try:
-		results.extend(mem.test(progConfig))
+		results.extend(mem.test(ALL_C['progConfig']))
 	except:
 		results.append('F','Memmory map command testing to FPGA test failed to run.')
 
@@ -87,7 +75,7 @@ if __name__ == "__main__":
 		
 	#Testing connection to bus as slave
 	try:
-		plBusSlave = comm.connection(plBus_vid,plBus_pid,plBus_packet_size,plBus_timeout, plBus_wendpoint, plBus_rendpoint)
+		plBusSlave = comm.connection(ALL_C['plBus_vid'],ALL_C['plBus_pid'],ALL_C['plBus_packet_size'],ALL_C['plBus_timeout'], ALL_C['plBus_wendpoint'], ALL_C['plBus_rendpoint'])
 		results.append(plBusSlave.testConnection(plBus_slave_sendTest, plBus_slave_expectTest))
 	except:
 		results.append('F','Connection to bus as slave failed')
--- UROP_NODE/errorDetect.py
+++ UROP_NODE/errorDetect.py
@@ -1,4 +1,5 @@
 import test.mmap_test.py as mmap
+from test.constants import ALL_C
 """
 This is to test the currents of devices during the alarms section of the cotrol code.
 It reports errors when devices are not cosuming the current they should be consuming.
@@ -7,8 +8,10 @@ It reports errors when devices are not cosuming the current they should be consu
 #this dictionary holds the locations to be tested along with their respective bounds for acceptable currents
 ##need to find the lower and upper bound current for each of these and then store the results in an error report
 bounds = {
-			'CC1a': (0,0), 'CC1b': (0,0), 'CC2a': (0,0), 'CC3a': (0,0), 'CC3b': (0,0), 'CC4a': (0,0),
-			'CC4b': (0,0), 'LCCa': (0,0), 'LCCb': (0,0)
+			'CC1a': (ALL_C['CC1a_LOW'],ALL_C['CC1a_HIGH']), 'CC1b': (ALL_C['CC1b_LOW'],ALL_C['CC1b_HIGH']), 'CC2a': (ALL_C['CC2a_LOW'],ALL_C['CC2a_HIGH']),
+			'CC2b': (ALL_C['CC2b_LOW'],ALL_C['CC2b_HIGH']), 'CC3a': (ALL_C['CC3a_LOW'],ALL_C['CC3a_HIGH']), 'CC3b': (ALL_C['CC3b_LOW'],ALL_C['CC3b_HIGH']),
+			'CC4a': (ALL_C['CC4a_LOW'],ALL_C['CC4a_HIGH']), 'CC4b': (ALL_C['CC4b_LOW'],ALL_C['CC4b_HIGH']), 'LCCa': (ALL_C['LCCa_LOW'],ALL_C['LCCa_HIGH']),
+			'LCCb': (ALL_C['LCCb_LOW'],ALL_C['LCCb_HIGH'])
 		}
 
 
@@ -22,9 +25,45 @@ def code2current(code):
 		current(double): current derived from code
 	"""
 	##need to implement converter from data read to current
+	##something like this
+	# def code2byte(code):
+ #    	fb = code/256
+ #    	sb = code%256
+ #    	return fb, sb
+	# def code2voltage(c):
+ #    	max_code = 2**12 #assuming 12-bit ADC
+ #    	V_cc = 3.3 #assuming 3.3V source
+ #    	return c*(V_cc/max_code)
+
+
 	pass
 
-def check_currents(old_vid_pid, new_vid_pid):
+def start(old_vid_pid, new_vid_pid):
+	m = mmap.tester(old_vid_pid, new_vid_pid) #initialize memmory map
+	return m
+
+def end(m):
+	m.end(m.fpga)
+
+
+def clock_cycles_since_reset(m, old_counter):
+	counter = m.read(m.fpga, m.get_addr('FRC'))
+	if old_counter == counter:
+		#'Error: counter since last reset has not changed, possible comm loss'
+		return counter
+	elif old_counter < counter:
+		#'Counter increased to: ', counter, 'delta: ', (counter- old_counter)
+		return 0
+	else:
+		#'Counter decreased to: ', counter, 'delta: ', (counter-old_counter) 
+		return 0
+
+def read_SEM(m):
+	flags = m.read(m.fpga, m.get_addr('SFL'))
+	status = m.read(m.fpga, m.get_addr('SST'))
+
+	return (flags, status)
+def check_currents(m):
 	"""
 	Checks that the currents are correct and returns report
 
@@ -36,14 +75,13 @@ def check_currents(old_vid_pid, new_vid_pid):
 		out_of_range(list): list of tuples with (location, current) if they are out of bounds
 	"""
 
-	m = mmap.tester(old_vid_pid, new_vid_pid) #initialize memmory map
+	
 	out_of_range = [] #array to hold out of range failures
 	for key in bounds:
 		data = m.read(m.fpga, m.get_addr(key))
 		current = code2current(data)
 		if current < bounds[key][0] or current > bounds[key][1]:
 			out_of_range.append((key, current))
-
 	if len(out_of_range) > 0:
 		return out_of_range
 	return 0
