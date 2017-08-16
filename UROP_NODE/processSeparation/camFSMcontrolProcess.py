c106682db2da86dad27a740df09225cf258be4db
Santiago Munoz <ryuxik@Santiagos-MacBook-Pro.local>
2017-08-16 11:25:22 -0400

    minor changes

--- UROP_NODE/processSeparation/camFSMcontrolProcess.py
+++ UROP_NODE/processSeparation/camFSMcontrolProcess.py
@@ -1,1 +1,9 @@
-#implement this next week after meeting with ondrej
+#This should look something like what is in wrappingTest, this is still in progress because the code for the tracking is still not 
+#finalized 
+
+# import camFSMcontrol
+
+# assert 'mainLoop' in dir(camFSMcontrol)
+# assert callable(camFSMcontrol.mainLoop)
+
+# camFSMcontrol.mainLoop()
